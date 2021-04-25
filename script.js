fd39266645dd847379d8cbb3dae4f44c58cf7d69
jdelacruzv <jldlcv@gmail.com>
2021-04-24 23:10:26 -0500

    The random quotes algorithm was modified

--- script.js
+++ script.js
@@ -4,27 +4,24 @@ let btnSearch = document.querySelector('.header__button');
 let btnRandom = document.querySelector('.header__random');
 
 // Conexión a la API 'quote-garden quotes'
-const getAllQuote = () => {
-    document.querySelector('.main__title').style.display = 'block';
+const getAuthorName = () => {
     if(search.value === '') {
         swal('Ingrese nombre del autor', '', 'error');
     } else {
-        // totalQuotes = 72672, totalAuthors = 11369
-        fetch(`https://quote-garden.herokuapp.com/api/v3/quotes?author=${search.value}`)
+        fetch(`https://quote-garden.herokuapp.com/api/v3/quotes?author=${search.value}&limit=4`)
         .then(response => response.json())
-        .then(quotes => showAllQuote(quotes))
+        .then(quotes => showAuthorQuotes(quotes))
         .catch(error => swal('El autor no existe!', '', 'error'))        
     }
+    search.value = '';
 }
 
 // Muestra los datos al dar click en el botón search
-const showAllQuote = quotes => {
-    console.log(quotes.data);
-    let author = document.querySelector('.main__title');
-    author.textContent = quotes.data[0].quoteAuthor;  
+const showAuthorQuotes = quotes => {
+    // console.log(quotes.data);
+    document.querySelector('.main__title').textContent = quotes.data[0].quoteAuthor;  
     let body = '';
     for(let i=0; i<quotes.data.length; i++) {
-        if(i<4) {
             body += `
             <div class="quotes">
                 <h3 class="quotes__title">${quotes.data[i].quoteGenre}</h3>
@@ -32,43 +29,46 @@ const showAllQuote = quotes => {
             </div>
             `;
             document.querySelector('.main__article').innerHTML = body;
-        }
     }
-    search.value = '';
 } 
 
 // Evento del botón 'btnSearch'
-btnSearch.addEventListener('click', getAllQuote);
+btnSearch.addEventListener('click', getAuthorName);
 
-// Conexión a la API 'quote-garden random'
-const getRandomQuote = () => {
-    fetch('https://quote-garden.herokuapp.com/api/v3/quotes/random?count=4')
+// Obtiene un número aleatorio de 1 al 11370
+const getRandomNumber = () => Math.floor((Math.random() * 11370) + 1);
+
+// Conexión a la API 'quote-garden authors' y 'quote-garden random'
+const getRandomAuthorName = () => {
+    fetch(`https://quote-garden.herokuapp.com/api/v3/authors`)
         .then(response => response.json())
-        .then(random => showRandomQuote(random))
-        .catch(error => console.log(error))
+        .then(authors => {
+            let name = authors.data[getRandomNumber()];
+            // console.log(name);
+            fetch(`https://quote-garden.herokuapp.com/api/v3/quotes/random?author=${name}&count=4`)
+                .then(response => response.json())
+                .then(random => showRandomQuote(random));
+        });
 }
 
 // Muestra los datos según el resultado del botón random 
 const showRandomQuote = random => {
-    document.querySelector('.main__title').textContent = 'Autores varios';
+    document.querySelector('.main__title').textContent = random.data[0].quoteAuthor;  
+    // console.log(random.data);
     let body = '';
     for(let i=0; i<random.data.length; i++) {
         body += `
         <div class="quotes">
             <h3 class="quotes__title">${random.data[i].quoteGenre}</h3>
             <p class="quotes__text">"${random.data[i].quoteText}"</p>
-            <p class="quotes__author">${random.data[i].quoteAuthor}</p>
         </div>
         `;
         document.querySelector('.main__article').innerHTML = body;
     }
-    loadPage();
-    // Establece display 'none' a la clase spinner
-    document.querySelector('.spinner').style.display = 'none';
 } 
 
 // Evento del botón 'btnRandom'
-btnRandom.addEventListener('click', getRandomQuote);
+btnRandom.addEventListener('click', getRandomAuthorName);
 
 // Carga la página llamando a la función quoteRandom()
-window.onload = getRandomQuote();
+window.onload = getRandomAuthorName();
